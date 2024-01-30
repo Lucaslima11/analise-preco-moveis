77356dc1e914f8ac0597bf4ae30a070dbea88731
Lucas Lima <lucaslima111@gmail.com>
2024-01-29 23:51:53 -0300

    feat: função de retorno básica de titulos da página

--- src/madeira_madeira.py
+++ src/madeira_madeira.py
@@ -3,13 +3,19 @@ from playwright.async_api import async_playwright
 
 async def main():
     async with async_playwright() as pw:
-        browser = await pw.firefox.launch(
+        browser = await pw.chromium.launch(
             headless=False
         )
 
         page = await browser.new_page()
-        await page.goto('https://www.globo.com')
-        await print(page.content())
+        await page.goto('https://www.madeiramadeira.com.br/moveis/moveis-para-quarto-infantil/cama-montessoriana')
+        await page.wait_for_timeout(5000)
+
+        all_products = await page.locator('//*[@id="control-box-content"]/div[3]/div[1]/div[2]/div/div/div/div[2]/div')\
+        .filter(has_text='Cama')\
+        .locator('h2').all_inner_texts()
+        print(all_products)
+        await browser.close()
 
 if __name__ == '__main__':
     asyncio.run(main())
